6d064c2e111f3d3930c824a4abb9ace413ad0951
Alican Kaylan <alicankaylan@gmail.com>
2020-05-20 21:30:08 +0300

    Create softmax.py

---
+++ softmax.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    l = np.exp(L)
+    return l /sum(l) 
